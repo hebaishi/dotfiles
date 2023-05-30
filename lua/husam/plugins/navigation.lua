73895fff92609ccde6a050e4a1eccaac683a0f51
husam-hebaishi <husam.hebaishi@vcatechnology.com>
2023-05-30 22:54:20 +0100

    Move telescope setup into config()

--- lua/husam/plugins/navigation.lua
+++ lua/husam/plugins/navigation.lua
@@ -28,13 +28,7 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-     end
-  },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-    config = function()
-      require('telescope').setup {
+      require('telescope').setup({
         extensions = {
           fzf = {
             fuzzy = true,                    -- false will only do exact matching
@@ -44,10 +38,14 @@ return {
                                              -- the default case_mode is "smart_case"
           }
         }
-      }
+      })
       -- To get fzf loaded and working with telescope, you need to call
       -- load_extension, somewhere after setup function:
       require('telescope').load_extension('fzf')
-    end
+     end
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
   },
 }
