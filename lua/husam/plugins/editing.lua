224da2b94ecdd762019db9d7793287d52555e811
husam-hebaishi <husam.hebaishi@vcatechnology.com>
2023-05-30 19:50:48 +0100

    Add debugging UI, surround and comment plugins

--- lua/husam/plugins/editing.lua
+++ lua/husam/plugins/editing.lua
@@ -2,6 +2,14 @@ return {
   "mg979/vim-visual-multi",
   "L3MON4D3/LuaSnip",
   {
+    "numToStr/Comment.nvim",
+    config = true
+  },
+  {
+    "kylechui/nvim-surround",
+    config = true
+  },
+  {
     "saadparwaiz1/cmp_luasnip",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load({ paths = { "/home/hebaishi/snippets" } })
