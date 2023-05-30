d9f1824bd2d035e14d82053b219e6be4d65a115c
husam-hebaishi <husam.hebaishi@vcatechnology.com>
2023-05-30 19:52:53 +0100

    Fix indentation in keymaps.lua

--- lua/husam/core/keymaps.lua
+++ lua/husam/core/keymaps.lua
@@ -30,36 +30,36 @@ vim.keymap.set('n', '<A-Down>', function()
 	]])
 end,{})
 vim.keymap.set('n', '<F12>', function()
-        vim.cmd(':cclose')
+  vim.cmd(':cclose')
 end,{})
 vim.keymap.set('n', '<Leader>db', function()
-        require("dapui").toggle()
+  require("dapui").toggle()
 end,{})
 vim.keymap.set('n', '<F8>', function()
-        local status_ok, _ = pcall( vim.cmd, ':cn')
-        if not status_ok then
-          vim.cmd(':crewind')
-          vim.cmd(':cn')
-        end
+  local status_ok, _ = pcall( vim.cmd, ':cn')
+  if not status_ok then
+    vim.cmd(':crewind')
+    vim.cmd(':cn')
+  end
 end,{})
 vim.keymap.set('n', '<F9>', function()
-        require('dap').toggle_breakpoint()
+  require('dap').toggle_breakpoint()
 end,{})
 vim.keymap.set('n', '<F10>', function()
-        require('dap').step_over()
+  require('dap').step_over()
 end,{})
 vim.keymap.set('n', '<F7>', function()
-        vim.cmd(':copen')
-        vim.cmd(':AsyncRun cmake --build build --target all')
+  vim.cmd(':copen')
+  vim.cmd(':AsyncRun cmake --build build --target all')
 end,{})
 vim.keymap.set('n', '<S-F7>', function()
-        vim.cmd(':cclose')
+  vim.cmd(':cclose')
 end,{})
 vim.keymap.set('n', '<Leader>tt', function()
-        vim.cmd([[
-        :bo split
-        :term
-        ]])
+  vim.cmd([[
+  :bo split
+  :term
+  ]])
 end,{})
 vim.keymap.set('n', 't', function ()
   local current_window = vim.fn.win_getid()
