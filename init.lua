18401afbc5b2aa1d537dec56cc49fa929f33c4c6
husam-hebaishi <husam.hebaishi@vcatechnology.com>
2023-06-26 17:26:05 +0100

    Improve diagnostic icons

--- init.lua
+++ init.lua
@@ -2,3 +2,14 @@ require('husam.core.options')
 require('husam.plugin-setup')
 require('husam.core.keymaps')
 require('husam.core.lsp')
+local signs = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Information = " "
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
