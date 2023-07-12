0b9e880fdeda7aac07cdc91c1907b71638113c99
Husam Hebaishi <hebaishi.husam@gmail.com>
2023-07-12 18:22:00 +0100

    Remove redundant fzf-native telescope extension

--- lua/husam/plugins/navigation.lua
+++ lua/husam/plugins/navigation.lua
@@ -39,14 +39,7 @@ return {
           }
         }
       })
-      -- To get fzf loaded and working with telescope, you need to call
-      -- load_extension, somewhere after setup function:
-      require('telescope').load_extension('fzf')
       require('telescope').load_extension('ui-select')
      end
-  },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-  },
+  }
 }
