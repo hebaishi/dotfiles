6f0f67701cda2d92b10db4db0518c680328a0e9b
Husam Hebaishi <hebaishi.husam@gmail.com>
2023-07-18 19:08:14 +0100

    Add indent-blankline plugin

--- lua/husam/plugins/graphical.lua
+++ lua/husam/plugins/graphical.lua
@@ -1,5 +1,26 @@
 return {
   {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      vim.opt.list = true
+      vim.opt.listchars:append "space:⋅"
+      vim.opt.listchars:append "eol:↴"
+      require("indent_blankline").setup({
+        show_end_of_line = true,
+        space_char_blankline = " ",
+        indent_blankline_filetype_exclude = {
+          "lspinfo",
+          "packer",
+          "checkhealth",
+          "help",
+          "man",
+          "Neogitstatus"
+        }
+      }
+    )
+    end
+  },
+  {
     "nvim-telescope/telescope-ui-select.nvim",
     config = true
   },
