c5fdcd2b46957f053c65521ec866be66bd96889c
Husam Hebaishi <hebaishi.husam@gmail.com>
2023-09-13 15:08:23 +0100

    Remove blankline plugin

--- lua/husam/plugins/graphical.lua
+++ lua/husam/plugins/graphical.lua
@@ -1,26 +1,5 @@
 return {
   {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-      vim.opt.list = true
-      vim.opt.listchars:append "space:⋅"
-      vim.opt.listchars:append "eol:↴"
-      require("indent_blankline").setup({
-        show_end_of_line = true,
-        space_char_blankline = " ",
-        indent_blankline_filetype_exclude = {
-          "lspinfo",
-          "packer",
-          "checkhealth",
-          "help",
-          "man",
-          "Neogitstatus"
-        }
-      }
-    )
-    end
-  },
-  {
     "stevearc/dressing.nvim",
     config = true
   },
