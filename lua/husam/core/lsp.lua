5d1660a82f3969d13c9d37a54fc2dd1e7533f08a
Husam Hebaishi <hebaishi.husam@gmail.com>
2023-09-21 18:58:07 +0100

    Move LSP diagnostic config to core/lsp.lua

--- lua/husam/core/lsp.lua
+++ lua/husam/core/lsp.lua
@@ -28,3 +28,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
   end,
 })
+local signs = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Information = " "
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
