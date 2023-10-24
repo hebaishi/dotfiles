3317f6e1d00d3cea4f4a7452d61bb6cceb0099e6
Husam Hebaishi <hebaishi.husam@gmail.com>
2023-10-24 10:12:44 +0100

    Fix AI keybinding conflicting with nvim-cmp

--- lua/husam/plugins/ai.lua
+++ lua/husam/plugins/ai.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require('tabnine').setup({
         disable_auto_comment = true,
-        accept_keymap = "<Tab>",
+        accept_keymap = "<C-a>",
         dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
