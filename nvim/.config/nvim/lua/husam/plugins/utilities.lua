a783ebec03b46f68f6862a0c8cd6c31e1816c5ce
Husam Hebaishi <hebaishi.husam@gmail.com>
2023-12-19 00:41:30 +0000

    Add keymap for TODO items

--- nvim/.config/nvim/lua/husam/plugins/utilities.lua
+++ nvim/.config/nvim/lua/husam/plugins/utilities.lua
@@ -1,6 +1,9 @@
 return {
   {
     "folke/todo-comments.nvim",
+    keys = {
+      { "<leader>td", "<cmd>TodoTelescope<cr>", desc = "Show TODO items" },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       'nvim-telescope/telescope.nvim'
