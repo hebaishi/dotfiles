eb4b388a3155da85210812c0edc9646f1f1954c1
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-02-14 13:27:57 +0000

    Add keymap for searching for a string

--- nvim/.config/nvim/lua/husam/plugins/navigation.lua
+++ nvim/.config/nvim/lua/husam/plugins/navigation.lua
@@ -29,6 +29,7 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
       require('telescope').setup({
         extensions = {
           fzf = {
