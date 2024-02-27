36e2ecc39577d4580923d781d24aa9013d6f8c71
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-02-27 20:58:11 +0000

    Add ollama.nvim for AI-powered editing

--- nvim/.config/nvim/lua/husam/plugins/editing.lua
+++ nvim/.config/nvim/lua/husam/plugins/editing.lua
@@ -1,5 +1,47 @@
 return {
   {
+    {
+      "nomnivore/ollama.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      -- All the user commands added by the plugin
+      cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+      keys = {
+        {
+          "<leader>oo",
+          ":<c-u>lua require('ollama').prompt()<cr>",
+          desc = "ollama prompt",
+          mode = { "n", "v" },
+        },
+        {
+          "<leader>oG",
+          ":<c-u>lua require('ollama').prompt('CodeGen2')<cr>",
+          desc = "ollama Generate Code",
+          mode = { "n", "v" },
+        },
+      },
+      opts = {
+        model = "codellama",
+        url = "http://127.0.0.1:11434",
+        serve = {
+          on_start = false,
+          command = "ollama",
+          args = { "serve" },
+          stop_command = "pkill",
+          stop_args = { "-SIGTERM", "ollama" },
+        },
+        prompts = {
+          CodeGen2 = {
+            prompt = "$input. Write all code in a code block. Do not explain the code.",
+            input_label = "> ",
+            model = "codellama",
+            action = "insert",
+            extract = "```%w*\n(.-)```"
+          }
+        }
+      }
+    },
     "sourproton/tunnell.nvim",
     opts = {
       -- defaults are:
