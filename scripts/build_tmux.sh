56a8439e679ea4813bf1ac26841daeb09314793c
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-02-28 01:12:29 +0000

    Fix tmux build script

--- scripts/build_tmux.sh
+++ scripts/build_tmux.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -e
+build_dir=~/dev/tmux
 tmux_version=3.3
 sudo apt-get update
-sudo apt-get install -y libncurses-dev libevent-dev build-essential
-git clone https://github.com/tmux/tmux
-cd tmux
+sudo apt-get install -y libevent-dev ncurses-dev build-essential bison pkg-config
+mkdir $build_dir
+git clone https://github.com/tmux/tmux $build_dir
+cd $build_dir
 git checkout $tmux_version
 sh autogen.sh
 ./configure
