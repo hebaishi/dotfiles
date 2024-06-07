3772fb79a3adc3aa863cbfbfdb15ae0522019eeb
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-06-07 01:42:11 +0100

    Format Telescope plugin table

--- nvim/.config/nvim/lua/husam/plugins/navigation.lua
+++ nvim/.config/nvim/lua/husam/plugins/navigation.lua
@@ -23,9 +23,10 @@ return {
     end,
   },
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-     dependencies = { 'nvim-lua/plenary.nvim' },
-     config = function()
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.4',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', function()
         builtin.find_files({ hidden = true })
@@ -35,14 +36,14 @@ return {
       require('telescope').setup({
         extensions = {
           fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                             -- the default case_mode is "smart_case"
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
           }
         }
       })
-     end
+    end
   }
 }
