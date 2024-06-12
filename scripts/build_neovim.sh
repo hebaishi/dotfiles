2a35caf350c28c667696c07caed5f93bfa68fc4e
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-06-12 23:31:14 +0100

    Move build type out into variable

--- scripts/build_neovim.sh
+++ scripts/build_neovim.sh
@@ -2,6 +2,7 @@
 set -e
 build_dir=~/dev/neovim
 version=v0.10.0
+build_type=Release
 
 if [ -d "$build_dir" ]; then
   cd $build_dir
@@ -14,9 +15,9 @@ fi
 cd $build_dir
 git checkout $version
 sudo apt-get install ninja-build gettext cmake unzip curl
-cmake -S cmake.deps -B .deps -G Ninja -D CMAKE_BUILD_TYPE=Release
+cmake -S cmake.deps -B .deps -G Ninja -D CMAKE_BUILD_TYPE=$build_type
 cmake --build .deps
-cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=Release
+cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=$build_type
 cmake --build build
 cd build
 cpack -G DEB
