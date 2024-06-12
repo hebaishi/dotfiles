7cb167576b1a3a2dc4cc544e02d7646293059f61
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-06-12 23:09:13 +0100

    Break up LSP plugins into separate files

---
+++ nvim/.config/nvim/lua/husam/plugins/cmp-nvim-lsp.lua
@@ -0,0 +1,7 @@
+return {
+  "hrsh7th/cmp-nvim-lsp",
+  config = function()
+    -- Set up lspconfig.
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  end
+}
---
+++ nvim/.config/nvim/lua/husam/plugins/nvim-lspconfig.lua
@@ -0,0 +1,64 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    vim.keymap.set('n', '<Leader>xx', function()
+      vim.cmd("w")
+      vim.cmd("source %")
+    end, {})
+    require 'lspconfig'.lua_ls.setup {
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    }
+    require('lspconfig').clangd.setup {
+      settings = {
+        ['clangd'] = {},
+      },
+    }
+    require 'lspconfig'.vtsls.setup {}
+    require 'lspconfig'.jsonls.setup {}
+    require 'lspconfig'.rust_analyzer.setup {}
+    require 'lspconfig'.pylsp.setup {
+      settings = {
+        pylsp = {
+          plugins = {
+            pycodestyle = {
+              ignore = {
+                'E111',
+                'E121',
+              },
+              maxLineLength = 120
+            }
+          }
+        }
+      }
+    }
+    require('lspconfig').yamlls.setup {
+      settings = {
+        yaml = {
+          schemas = {
+            ["https://gitlab.com/gitlab-org/gitlab/-/raw/v14.10.0-ee/app/assets/javascripts/editor/schema/ci.json?ref_type=tags"] = ".gitlab-ci.yml"
+          },
+        },
+      }
+    }
+    require 'lspconfig'.bashls.setup {}
+  end
+}
