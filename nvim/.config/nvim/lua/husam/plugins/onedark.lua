f5ac81450364e4833df7eb83856904971c8246bd
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-06-20 21:10:15 +0100

    Use 'darker' variant of onedark theme

--- nvim/.config/nvim/lua/husam/plugins/onedark.lua
+++ nvim/.config/nvim/lua/husam/plugins/onedark.lua
@@ -2,7 +2,7 @@ return {
   "navarasu/onedark.nvim",
   config = function()
     require("onedark").setup({
-      style = 'deep'
+      style = 'darker'
     })
     require('onedark').load()
     vim.cmd.colorscheme = "onedark"
