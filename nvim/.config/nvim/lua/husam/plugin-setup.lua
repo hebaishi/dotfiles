5fd812518c2375736957c255705ce659418cc5f1
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-07-15 03:24:27 +0100

    Use nice icons for lazy UI

--- nvim/.config/nvim/lua/husam/plugin-setup.lua
+++ nvim/.config/nvim/lua/husam/plugin-setup.lua
@@ -11,4 +11,22 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("husam.plugins")
+require("lazy").setup("husam.plugins", {
+  ui = {
+    icons = {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
