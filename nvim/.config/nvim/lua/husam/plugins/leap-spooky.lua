f8107079a09b1704762c76babeaee268ca90f2af
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-08-21 22:30:39 +0100

    Remove leap and leap-spooky

--- nvim/.config/nvim/lua/husam/plugins/leap-spooky.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ggandor/leap-spooky.nvim",
-  config = true,
-  dependencies = {
-    "ggandor/leap.nvim"
-  }
-}
--- nvim/.config/nvim/lua/husam/plugins/leap.lua
+++ nvim/.config/nvim/lua/husam/plugins/leap.lua
@@ -1,10 +1,14 @@
 return {
-  "ggandor/leap.nvim",
-  config = function()
-    require('leap').add_default_mappings()
-    vim.keymap.set('n', 't', function()
-      local current_window = vim.fn.win_getid()
-      require('leap').leap { target_windows = { current_window } }
-    end)
-  end
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
 }
