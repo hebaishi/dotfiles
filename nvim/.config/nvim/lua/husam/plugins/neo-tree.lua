164d029693cd3c463f461abdf3781ee338dadccb
Husam Hebaishi <hebaishi.husam@gmail.com>
2024-09-10 22:30:46 +0100

    Add copy to clipboard function for tree

--- nvim/.config/nvim/lua/husam/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/husam/plugins/neo-tree.lua
@@ -1,6 +1,44 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
+local function copy_path(state)
+  -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+  -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+  local node = state.tree:get_node()
+  local filepath = node:get_id()
+  local filename = node.name
+  local modify = vim.fn.fnamemodify
 
+  local results = {
+    filepath,
+    modify(filepath, ":."),
+    modify(filepath, ":~"),
+    filename,
+    modify(filename, ":r"),
+    modify(filename, ":e"),
+  }
+
+  vim.ui.select({
+    "1. Absolute path: " .. results[1],
+    "2. Path relative to CWD: " .. results[2],
+    "3. Path relative to HOME: " .. results[3],
+    "4. Filename: " .. results[4],
+    "5. Filename without extension: " .. results[5],
+    "6. Extension of the filename: " .. results[6],
+  }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+    if choice then
+      local i = tonumber(choice:sub(1, 1))
+      if i then
+        local result = results[i]
+        vim.fn.setreg('"', result)
+        vim.notify("Copied: " .. result)
+      else
+        vim.notify("Invalid selection")
+      end
+    else
+      vim.notify("Selection cancelled")
+    end
+  end)
+end
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -17,6 +55,7 @@ return {
     filesystem = {
       window = {
         mappings = {
+          ['Y'] = copy_path,
           ['\\'] = 'close_window',
           ["<tab>"] = "open",
         },
