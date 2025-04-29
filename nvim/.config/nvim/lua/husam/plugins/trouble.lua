6f7d6fffe6b22dc226af4aeeb6c1b5268d8fe4ac
Husam Hebaishi <hebaishi.husam@gmail.com>
2025-04-29 15:27:12 +0100

    Add trouble for easier LSP error nagivation

---
+++ nvim/.config/nvim/lua/husam/plugins/trouble.lua
@@ -0,0 +1,27 @@
+return {
+  "folke/trouble.nvim",
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0 filter.severity=vim.diagnostic.severity.ERROR<cr>",
+      desc = "Buffer Errors (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    }
+  }
+}
