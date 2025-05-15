d1eaf55e1dafb5d044b6cb453b39a3831cdc4e02
Husam Hebaishi <hebaishi.husam@gmail.com>
2025-05-15 18:33:37 +0100

    Enable autoclosing tags in JS code

--- nvim/.config/nvim/lua/husam/plugins/nvim-ts-autotag.lua
+++ nvim/.config/nvim/lua/husam/plugins/nvim-ts-autotag.lua
@@ -1,4 +1,14 @@
 return {
   "windwp/nvim-ts-autotag",
-  config = true
+  config = function()
+    require('nvim-ts-autotag').setup({
+      filetypes = {
+        'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'xml',
+      },
+      skip_tags = {
+        'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input',
+        'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr', 'menuitem'
+      }
+    })
+  end
 }
