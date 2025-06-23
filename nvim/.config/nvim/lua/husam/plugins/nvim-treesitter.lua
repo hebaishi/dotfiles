7cbece2ae61c2b74593a3d59820718a8ba1c813a
Husam Hebaishi <hebaishi.husam@gmail.com>
2025-06-23 16:46:07 +0100

    Enable highlights for treesitter

--- nvim/.config/nvim/lua/husam/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/husam/plugins/nvim-treesitter.lua
@@ -1,3 +1,10 @@
 return {
-  "nvim-treesitter/nvim-treesitter"
+  "nvim-treesitter/nvim-treesitter",
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+      highlight = {
+        enable = true,
+      },
+    }
+  end
 }
