1db6172d2de07620a5867bb712f71bb324b8f06b
Husam Hebaishi <hebaishi.husam@gmail.com>
2025-06-23 17:45:35 +0100

    Supercharge nvim-dap-ui
    * Make the debugger UI automatically appear and disappear depending on
    whether a debug session is active
    * When activated, close file tree and quickfix windows for tidiness
    * Update to the latest version to fix the window moving bug

--- nvim/.config/nvim/lua/husam/plugins/nvim-dap-ui.lua
+++ nvim/.config/nvim/lua/husam/plugins/nvim-dap-ui.lua
@@ -54,10 +54,10 @@ return {
         size = 90
       }, {
         elements = { {
-          id = "repl",
+          id = "console",
           size = 0.5
         }, {
-          id = "console",
+          id = "repl",
           size = 0.5
         } },
         position = "bottom",
@@ -76,8 +76,19 @@ return {
         max_value_lines = 100
       }
     })
-    vim.keymap.set('n', '<Leader>db', function()
-      require("dapui").toggle()
-    end, { desc = "Start debugging" })
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      vim.cmd(':cclose')
+      vim.cmd(':Neotree close')
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
   end
 }
