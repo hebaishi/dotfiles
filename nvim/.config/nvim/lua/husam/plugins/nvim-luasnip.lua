fcb61334e63b4e81bb3b282b9fbe5189b8f5393c
Husam Hebaishi <hebaishi.husam@gmail.com>
2026-05-22 16:06:40 +0100

    Fix broken vscode snippets

--- nvim/.config/nvim/lua/husam/plugins/nvim-luasnip.lua
+++ nvim/.config/nvim/lua/husam/plugins/nvim-luasnip.lua
@@ -2,5 +2,9 @@ return {
   "saadparwaiz1/cmp_luasnip",
   config = function()
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { "/home/hebaishi/snippets" } })
+    local vscode_snippets = vim.fn.getcwd() .. "/.vscode/cpp.code-snippets"
+    if vim.fn.filereadable(vscode_snippets) == 1 then
+      require("luasnip.loaders.from_vscode").load_standalone({ path = vscode_snippets })
+    end
   end
 }
