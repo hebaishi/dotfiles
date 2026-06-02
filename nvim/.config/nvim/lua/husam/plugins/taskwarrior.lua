b641c60fd87289420de9fd730b52f07267e22709
Husam Hebaishi <hebaishi.husam@gmail.com>
2026-06-02 21:15:32 +0100

    Remove taskwarrior.nvim

--- nvim/.config/nvim/lua/husam/plugins/taskwarrior.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "hebaishi/taskwarrior.nvim",
-  config = function()
-    vim.keymap.set("n", "<leader>tw", function()
-      require("taskwarrior_nvim").browser({ "ready" })
-    end, { desc = "TaskWarrior browser (ready)" })
-  end,
-}
