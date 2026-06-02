17ee64f8e8e9b339a50e42884bd6a2c9ce0b2eb7
Husam Hebaishi <hebaishi.husam@gmail.com>
2026-06-02 20:59:02 +0100

    Replace tiny-inline-diagnostic.nvim with builtin diagnostic display

--- nvim/.config/nvim/lua/husam/core/lsp.lua
+++ nvim/.config/nvim/lua/husam/core/lsp.lua
@@ -38,3 +38,4 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
 end
+vim.diagnostic.config({ virtual_lines = { current_line = true } })
