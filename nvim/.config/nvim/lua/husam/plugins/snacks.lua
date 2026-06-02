adb35c99f29c3c39814a5bf13c22a83d92781246
Husam Hebaishi <hebaishi.husam@gmail.com>
2026-06-02 21:03:45 +0100

    Replace nvim-notify with snacks.nvim

--- nvim/.config/nvim/lua/husam/plugins/snacks.lua
+++ nvim/.config/nvim/lua/husam/plugins/snacks.lua
@@ -2,10 +2,7 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    lazygit = {
-      -- your lazygit configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
+    notifier = { enabled = true },
+    lazygit = {},
   }
 }
