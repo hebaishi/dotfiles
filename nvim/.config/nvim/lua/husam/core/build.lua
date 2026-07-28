eef38afca0b08c744ea5d5f525ffb5dd0e97e09e
Husam Hebaishi <hebaishi.husam@gmail.com>
2026-07-28 15:15:52 +0900

    Refactor duplicated code in build.lua

--- nvim/.config/nvim/lua/husam/core/build.lua
+++ nvim/.config/nvim/lua/husam/core/build.lua
@@ -9,6 +9,19 @@ local function split_lines(data)
   return lines
 end
 
+local function make_output_handler(current_id)
+  return function(_, data)
+    if data and run_id == current_id then
+      vim.schedule(function()
+        if run_id == current_id then
+          vim.fn.setqflist({}, "a", { lines = split_lines(data) })
+          vim.cmd("cbottom")
+        end
+      end)
+    end
+  end
+end
+
 local function async_run(cmd)
   if job then
     job:kill(9)
@@ -19,27 +32,10 @@ local function async_run(cmd)
   vim.fn.setqflist({}, "r", { title = cmd, items = {} })
   vim.cmd("cbottom")
   local start = vim.uv.hrtime()
+  local handle_output = make_output_handler(current_id)
   job = vim.system({ "sh", "-c", cmd }, {
-    stdout = function(_, data)
-      if data and run_id == current_id then
-        vim.schedule(function()
-          if run_id == current_id then
-            vim.fn.setqflist({}, "a", { lines = split_lines(data) })
-            vim.cmd("cbottom")
-          end
-        end)
-      end
-    end,
-    stderr = function(_, data)
-      if data and run_id == current_id then
-        vim.schedule(function()
-          if run_id == current_id then
-            vim.fn.setqflist({}, "a", { lines = split_lines(data) })
-            vim.cmd("cbottom")
-          end
-        end)
-      end
-    end,
+    stdout = handle_output,
+    stderr = handle_output,
   }, function(result)
     if run_id == current_id then
       local elapsed = (vim.uv.hrtime() - start) / 1e9
